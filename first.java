ae2b93f2c4c7fb8db2b6da582a8bd68ac8c5a512
Kushal <duwadikushal99@gmail.com>
2025-07-19 14:38:41 +0545

    2ns update

--- first.java
+++ first.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
         System.out.println("Hello, Kushal!");
+        System.out.print("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("My name is " +  input.nextLine());
+
+
     }
 }
