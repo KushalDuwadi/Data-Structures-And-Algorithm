8309eae8dc0e7d150da5943b42fade2490784740
Kushal <duwadikushal99@gmail.com>
2025-07-21 21:32:56 +0545

    two dim array

---
+++ ArraysQ.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class ArraysQ {
+    public static void main(String[] args){
+        int[] marks = new  int[5];
+        Scanner input = new Scanner(System.in);
+        for(int i =0; i<marks.length; i++){
+            int mark = input.nextInt();
+            marks[i] = mark;
+
+        }
+        //displaying the array of marks
+        for(int i = 0; i<marks.length; i++){
+            // System.out.print("  " + marks[i]);
+            System.out.print(Arrays.toString(marks));
+        }
+    }
+}
